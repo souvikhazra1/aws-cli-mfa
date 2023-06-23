fd4519661000b7ca398f163034afeb4150e8d7ca
Souvik Hazra <souvik.babanhazra@gmail.com>
2023-06-24 00:19:39 +0530

    display qr in cli

--- src/enroll-mfa.ts
+++ src/enroll-mfa.ts
@@ -1,7 +1,10 @@
 import { IAM } from "@aws-sdk/client-iam";
-import fs from 'fs';
-import os from 'os';
-import path from 'path';
+import fs from "fs";
+import Jimp from "jimp";
+import jsQR from "jsqr";
+import os from "os";
+import path from "path";
+import qrGenerator from 'qrcode-terminal';
 import { question } from "readline-sync";
 
 const enrollMfa = async (credentials: any) => {
@@ -16,9 +19,21 @@ const enrollMfa = async (credentials: any) => {
         if (!resp.VirtualMFADevice) {
             throw '[E] Failed to create MFA device';
         }
-        const qrFile = path.join(os.tmpdir(), 'aws-mfa-qr.png');
-        fs.writeFileSync(qrFile, Buffer.from(resp.VirtualMFADevice.QRCodePNG));
-        console.log('QR File: ', qrFile);
+
+        try {
+            const img = await Jimp.read(Buffer.from(resp.VirtualMFADevice.QRCodePNG));
+            const qrData = jsQR(img.bitmap.data as never, img.bitmap.width, img.bitmap.height);
+            console.log();
+            qrGenerator.generate(qrData.data, { small: true });
+            console.log();
+        } catch {
+            console.log('[E] Failed to display the QR in CLI, saving as an image....');
+            // save the qr to file
+            const qrFile = path.join(os.tmpdir(), 'aws-mfa-qr.png');
+            fs.writeFileSync(qrFile, Buffer.from(resp.VirtualMFADevice.QRCodePNG));
+            console.log('QR File: ', qrFile);
+        }
+
         const code1 = question('Code 1: ');
         const code2 = question('Code 2: ');
         try {
